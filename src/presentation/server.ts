061b8c5cdbb1b7ff6a7bf5a0d3406a2f86c90e89
miguelbaumann1981 <miguelmartinezm@outlook.es>
2025-12-16 14:59:03 +0100

    working on REST server

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -29,6 +29,17 @@ export class Server {
         //--- Public folder
         this.app.use(express.static(this.publicPath));
 
+        //--- Routes
+        this.app.get('/api/todos', (req, res) => {
+            return res.json([
+                { id: 1, text: 'Buy milk', createdAt: new Date() },
+                { id: 2, text: 'Buy bread', createdAt: null },
+                { id: 3, text: 'Buy butter', createdAt: new Date() },
+            ])
+        })
+
+
+        // --- SPA
         this.app.get(/.*/, (req, res) => {
             const indexPath = path.join(__dirname + `../../../${this.publicPath}/index.html`);
             res.sendFile(indexPath);
