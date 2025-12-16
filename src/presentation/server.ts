f26e9c4085d9333295ab699d515829a718170fd8
miguelbaumann1981 <miguelmartinezm@outlook.es>
2025-12-16 17:25:00 +0100

    api works

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,9 +1,10 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
     public_path?: string;
+    routes: Router;
 }
 
 
@@ -12,31 +13,28 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, public_path = 'public', routes } = options;
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
 
     async start() {
 
         //--- Middlewares
-
+        this.app.use(express.json()); // raw
+        this.app.use(express.urlencoded({extended: true})); // w-www-form-urlencoded
 
 
         //--- Public folder
         this.app.use(express.static(this.publicPath));
 
         //--- Routes
-        this.app.get('/api/todos', (req, res) => {
-            return res.json([
-                { id: 1, text: 'Buy milk', createdAt: new Date() },
-                { id: 2, text: 'Buy bread', createdAt: null },
-                { id: 3, text: 'Buy butter', createdAt: new Date() },
-            ])
-        })
+        this.app.use(this.routes);
 
 
         // --- SPA
